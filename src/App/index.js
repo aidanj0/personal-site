be084f26b276adc33d3a4dbeeadcdf1ded5a400e
aidanj0 <aidanjohansson01@gmail.com>
2022-06-22 15:08:36 -0700

    placeholder info

--- src/App/index.js
+++ src/App/index.js
@@ -1,10 +1,11 @@
-
+import './index.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-      </header>
+    <div>
+      <h2>Hi there!</h2>
+      <p>The website is currently under construction as I replace my old portfolio site</p>
+      <p>Please feel free to reach out at aidanjohansson01@gmail.com in the meantime</p>
     </div>
   );
 }
