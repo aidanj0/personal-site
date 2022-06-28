78df40c2de34845c79efa058c4efa06394f49d69
aidanj0 <aidanjohansson01@gmail.com>
2022-06-27 19:47:28 -0700

    navbar styling updated

--- src/App/components/NavBar.js
+++ src/App/components/NavBar.js
@@ -6,9 +6,9 @@ const NavBar = () => (
     <div className="flex-container">
         <div className ="flex-item flex-item-main"><a href = '../'>AIDAN JOHANSSON</a></div>
         <div className ="flex-item flex-item-rest"><a href = '/about'>ABOUT</a></div>
-        <div className ="flex-item flex-item-rest"><a href = '/resume'>RESUME</a></div>
-        <div className ="flex-item flex-item-rest"><a href = '/projects'>PROJECTS</a></div>
-        <div className ="flex-item flex-item-rest"><a href = '/contact'>CONTACT</a></div>
+        <div className ="flex-item flex-item-rest non-second"><a href = '/resume'>RESUME</a></div>
+        <div className ="flex-item flex-item-rest non-second"><a href = '/projects'>PROJECTS</a></div>
+        <div className ="flex-item flex-item-rest non-second"><a href = '/contact'>CONTACT</a></div>
     </div>
 )
 
