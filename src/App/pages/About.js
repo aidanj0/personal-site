15d61a804720e622d2dfc0485a0c2d8eb00f64e1
aidanj0 <aidanjohansson01@gmail.com>
2022-07-24 16:36:37 -0700

    updated about text

--- src/App/pages/About.js
+++ src/App/pages/About.js
@@ -4,13 +4,15 @@ import Body from '../components/Body'
 
 import './main.css'
 
+const blurb = "I grew up near Phoenix, AZ, spending much of my time running with my high school's cross country and track & field teams and volunteering with local youth groups. I'm currently studying at Northeastern University, pursuing a Data Science major with a range of software and data-oriented coursework. During the fall semester, I'm working as a Data Science Co-op at CDP, working to apply data science tools to the betterment of public media."
+
 const About = () => (
     <Body>
         <div className='topdiv'>
             <h1>ABOUT ME</h1>
         </div>
         <div className='middiv'>
-            <p>I'm a third year Computer Science student at Northeastern University.</p>
+            <p>{blurb}</p>
         </div>
     </Body>
 )
